bb02c20f130a9a1a81ebdee2e45ea8fb3397f00d
Manish Jajoriya <manishjajoriya2005@gmail.com>
2026-01-21 17:18:45 +0530

    increase snap layer angle to 5f

--- app/src/main/java/com/ruhaan/orangeeditor/Constant.kt
+++ app/src/main/java/com/ruhaan/orangeeditor/Constant.kt
@@ -2,5 +2,5 @@ package com.ruhaan.orangeeditor
 
 object Constant {
   const val MAX_DIMENSION = 4096
-  const val LAYER_ROTATION_SNAP_THRESHOLD = 4f
+  const val LAYER_ROTATION_SNAP_THRESHOLD = 5f
 }
