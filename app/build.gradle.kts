9f617538b7a221618870cc8b85c6f76038b6d0c2
Manish Jajoriya <manishjajoriya2005@gmail.com>
2026-03-26 23:42:49 +0530

    refactor: change versionName and set isMinifyEnabled to false for debug build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,84 +1,83 @@
 plugins {
-  alias(libs.plugins.android.application)
-  alias(libs.plugins.kotlin.android)
-  alias(libs.plugins.kotlin.compose)
-  id("com.google.devtools.ksp")
-  id("com.google.dagger.hilt.android")
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
-  namespace = "com.ruhaan.orangeeditor"
-  compileSdk = 36
+    namespace = "com.ruhaan.orangeeditor"
+    compileSdk = 36
 
-  defaultConfig {
-    applicationId = "com.ruhaan.orangeeditor"
-    minSdk = 29
-    targetSdk = 36
-    versionCode = 1
-    versionName = "1.0"
+    defaultConfig {
+        applicationId = "com.ruhaan.orangeeditor"
+        minSdk = 29
+        targetSdk = 36
+        versionCode = 1
+        versionName = "0.1.0"
 
-    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-  }
-
-  buildTypes {
-    debug {
-      isMinifyEnabled = true
-      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-    release {
-      isMinifyEnabled = true
-      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+
+    buildTypes {
+        debug {
+            isMinifyEnabled = false
+        }
+        release {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
     }
-  }
-  compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
-  }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
 
-  // Commented because if needed in future testing
-  //  kotlin {
-  //    compilerOptions {
-  //      jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
-  //    }
-  //  }
+    // Commented because if needed in future testing
+    //  kotlin {
+    //    compilerOptions {
+    //      jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+    //    }
+    //  }
 
-  buildFeatures { compose = true }
+    buildFeatures { compose = true }
 }
 
 //noinspection UseTomlInstead
 dependencies {
-  implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.4")
-  implementation("com.github.Tanish-Ranjan:crop-kit:1.1.0")
-  implementation("com.github.KvColorPalette:KvColorPicker-Android:3.0.1")
-  implementation("androidx.compose.animation:animation:1.10.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.4")
+    implementation("com.github.Tanish-Ranjan:crop-kit:1.1.0")
+    implementation("com.github.KvColorPalette:KvColorPicker-Android:3.0.1")
+    implementation("androidx.compose.animation:animation:1.10.0")
 
-  // Room
-  implementation("androidx.room:room-runtime:2.8.4")
-  ksp("androidx.room:room-compiler:2.8.4")
+    // Room
+    implementation("androidx.room:room-runtime:2.8.4")
+    ksp("androidx.room:room-compiler:2.8.4")
 
-  // Dagger Hilt
-  implementation("com.google.dagger:hilt-android:2.57.2")
-  ksp("com.google.dagger:hilt-android-compiler:2.57.2")
-  implementation("androidx.hilt:hilt-navigation-compose:1.3.0")
+    // Dagger Hilt
+    implementation("com.google.dagger:hilt-android:2.57.2")
+    ksp("com.google.dagger:hilt-android-compiler:2.57.2")
+    implementation("androidx.hilt:hilt-navigation-compose:1.3.0")
 
-  // Coil Image
-  implementation("io.coil-kt.coil3:coil-compose:3.3.0")
+    // Coil Image
+    implementation("io.coil-kt.coil3:coil-compose:3.3.0")
 
-  implementation(libs.androidx.core.ktx)
-  implementation(libs.androidx.lifecycle.runtime.ktx)
-  implementation(libs.androidx.activity.compose)
-  implementation(platform(libs.androidx.compose.bom))
-  implementation(libs.androidx.ui)
-  implementation(libs.androidx.ui.graphics)
-  implementation(libs.androidx.ui.tooling.preview)
-  implementation(libs.androidx.material3)
-  implementation(libs.androidx.navigation.compose)
-  testImplementation(libs.junit)
-  androidTestImplementation(libs.androidx.junit)
-  androidTestImplementation(libs.androidx.espresso.core)
-  androidTestImplementation(platform(libs.androidx.compose.bom))
-  androidTestImplementation(libs.androidx.ui.test.junit4)
-  androidTestImplementation(libs.androidx.navigation.testing)
-  debugImplementation(libs.androidx.ui.tooling)
-  debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.navigation.compose)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    androidTestImplementation(libs.androidx.navigation.testing)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
 }
